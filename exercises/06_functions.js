5222cdbdec3d6bc11710972d7a331b12ee797ac7
unknown <valen1984@gmail.com>
2022-05-24 01:39:25 -0300

    EXERCISES: Corrección ejercicio 6C

--- exercises/06_functions.js
+++ exercises/06_functions.js
@@ -29,9 +29,33 @@ function Ejercicio6B() {
         }
     }
 
-function Ejercicio6C() {
-    let Valor1 = prompt("Ingresa el primer valor para la suma");
-    let Valor2 = prompt("Ingresa el segundo valor");
-    let total = Math.round(Number(Valor1) + Number(Valor2));
-    alert("La suma es " + total);
-}
+    function Ejercicio6C() {
+  
+        let variable1 = prompt("Ingrese el primer parametro");
+        let variable2 = prompt("Ingrese el segundo parametro");
+        var variable3 = variable1 + variable2;
+        let error = 'NaN';
+    
+        if (isNaN(variable1)) {
+            alert ("El parametro 1 tiene un error, el resultado es: " + error);
+        } 
+        else if (isNaN(variable2)) {
+                alert ("El parametro 2 tiene un error, el resultado es: " + error);
+        }    
+        else {
+            if (Number.isInteger(Number(variable1))) {
+                if (Number.isInteger(Number(variable2))) {
+                    variable3 = Number(variable1) + Number(variable2);
+                    alert("La suma es " + variable3);
+                } else {
+                        variable2 = Math.round(variable2)
+                        alert ("El parametro 2 no es entero, el numero redondeado es: " + variable2 );
+                }
+            } 
+            else {
+                variable1 = Math.round(variable1)
+                alert ("El parametro 1 no es entero, el numero redondeado es: " + variable1 );
+            }
+                
+        }
+        }
